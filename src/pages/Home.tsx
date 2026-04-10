41c74d0e9b9b61afd6609302ab2aa85fad9f2094
narek <narekhunanyan0497@gmail.com>
2026-04-10 12:38:15 +0400

    fix hom tsx and css

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,17 +12,21 @@ export default function Home() {
                 <div className="home-hero__content">
                     <span className="home-badge">HAYFIN</span>
 
-                    <h1>
+                    <h1 className="home-title">
                         Բարի գալուստ{user?.firstName ? `, ${user.firstName}` : ''}
                     </h1>
 
-                    <p>
+                    <p className="home-description">
                         Արագ, հարմար և վստահելի ֆինանսական լուծումներ՝ հենց քեզ համար։
                     </p>
 
                     <div className="home-hero__actions">
-                        <button className="home-btn home-btn--primary">Ստանալ առաջարկ</button>
-                        <button className="home-btn home-btn--ghost">Իմանալ ավելին</button>
+                        <button className="home-btn home-btn--primary">
+                            Ստանալ առաջարկ
+                        </button>
+                        <button className="home-btn home-btn--ghost">
+                            Իմանալ ավելին
+                        </button>
                     </div>
                 </div>
             </main>
